66492d4f90cd34ed56400663b7b068d7c4b02814
+ <andrewjohnjulian@gmail.com>
2023-05-13 22:04:23 -0600

    compenents folder and sample component added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import circleLogo from "/dblogo-whitecircle.png";
+import Navbar from "./Components/Navbar";
 import "./App.css";
 
 function App() {
   return (
     <>
       <div className="align-center justify-center">
+        <Navbar />
         <img src={circleLogo} className="App-logo" alt="logo" />
       </div>
     </>
