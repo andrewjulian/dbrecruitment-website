8ec83b58a66b8bb3865f83ab910ad83f13ee75ce
+ <andrewjohnjulian@gmail.com>
2023-05-17 17:36:48 -0600

    underline hover feature on navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -13,40 +13,40 @@ const Navbar = () => {
           />
         </div>
 
-        <div className="flex justify-center my-auto mr-5">
+        <div className="flex justify-center my-auto mr-5 ">
           <Link
             to="/"
-            className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+            className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-lg font-medium"
           >
             Home
           </Link>
           <Link
             to="/about"
-            className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+            className="text-white hover:text-gray-300 hover:border-b hover:border-[#6b6ff] px-3 py-2  text-lg font-medium"
           >
             About
           </Link>
           <Link
             to="/services"
-            className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+            className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-lg font-medium"
           >
             Services
           </Link>
           <Link
             to="/team"
-            className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+            className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-lg font-medium"
           >
             Team
           </Link>
           <Link
             to="/testimonials"
-            className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+            className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-lg font-medium"
           >
             Testimonials
           </Link>
           <Link
             to="/contact"
-            className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+            className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-lg font-medium"
           >
             Contact
           </Link>
