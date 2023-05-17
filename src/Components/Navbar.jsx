e77064ad57e4b3e34689379017f6499bb5edb2d0
+ <andrewjohnjulian@gmail.com>
2023-05-17 17:28:51 -0600

    basic navbar setup

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,59 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <div className="flex justify-between bg-gray-800">
+        <div className="flex justify-center p-5">
+          <img
+            src="/dblogo-whitecircle.png"
+            className="h-[50px] w-auto"
+            alt="logo"
+          />
+        </div>
+
+        <div className="flex justify-center my-auto mr-5">
+          <Link
+            to="/"
+            className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+          >
+            Home
+          </Link>
+          <Link
+            to="/about"
+            className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+          >
+            About
+          </Link>
+          <Link
+            to="/services"
+            className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+          >
+            Services
+          </Link>
+          <Link
+            to="/team"
+            className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+          >
+            Team
+          </Link>
+          <Link
+            to="/testimonials"
+            className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+          >
+            Testimonials
+          </Link>
+          <Link
+            to="/contact"
+            className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+          >
+            Contact
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
