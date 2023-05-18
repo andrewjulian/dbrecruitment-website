9cf9a349fe4ca518470bdc1f38e249b224f0a4a3
+ <andrewjohnjulian@gmail.com>
2023-05-18 12:54:05 -0600

    hero template setup

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,18 +4,30 @@ const Hero = () => {
   return (
     <>
       <div className="bg-white dark:bg-gray-900">
-        <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div className="flex max-w-screen-xl px-4 py-8 mx-auto mt-10">
           <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
               Empower Your Business With Our Top Talent
             </h1>
-            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+            <p className="max-w-2xl mb-2  text-black lg:mb-2 md:text-lg lg:text-xl">
               DB Recruitment Group specializes in connecting top cantidates with
               companies in the Cybersecruity, MSP, and SaaS industries.
             </p>
+            <p className="max-w-2xl mb-4 font-light italic text-gray-500 lg:mb-4 md:text-md lg:text-lg dark:text-gray-400">
+              We are proud to be a part of the tech community and are committed
+              to helping our clients and candidates succeed. We believe that by
+              working together, we can make a positive impact on the tech
+              industry.
+            </p>
             <a
               href="#"
-              className="inline-flex items-center justify-center px-5 py-3 mx-3 text-base font-medium text-center text-white bg-[#36b6ff] rounded-xl"
+              className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white hover:text-black bg-[#36b6ff] rounded-xl"
+            >
+              Meet The Team
+            </a>
+            <a
+              href="#"
+              className="inline-flex items-center hover:text-black justify-center px-5 py-3 mx-3 text-base font-medium text-center text-white bg-[#36b6ff] rounded-xl"
             >
               Contact Us
               <svg
@@ -32,11 +44,8 @@ const Hero = () => {
               </svg>
             </a>
           </div>
-          <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-            <img
-              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png"
-              alt="mockup"
-            />
+          <div className="lg:mt-2 lg:flex">
+            <img className="rounded-xl" src="/hero.jpeg" alt="mockup" />
           </div>
         </div>
       </div>
