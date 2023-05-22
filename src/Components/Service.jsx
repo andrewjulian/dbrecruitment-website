79c19d36c2da710577ab853df3275fe3b7c29480
+ <andrewjohnjulian@gmail.com>
2023-05-22 10:00:48 -0600

    accordian structure setup for services

--- src/Components/Service.jsx
+++ src/Components/Service.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 
-const Service = () => {
-  return <div>Service</div>;
+const Service = ({ title, content }) => {
+  const [isOpened, setOpened] = useState(false);
+  const [height, setHeight] = useState("0px");
+  const contentElement = useRef(null);
+
+  useEffect(() => {
+    setHeight(isOpened ? `${contentElement.current.scrollHeight}px` : "0px");
+  }, [isOpened]);
+
+  const handleOpening = () => {
+    setOpened(!isOpened);
+  };
+
+  return (
+    <div onClick={handleOpening} className="border border-indigo-400">
+      <div className="flex justify-between bg-indigo-300 p-4 text-white">
+        <h4 className="font-semibold">{title}</h4>
+        {isOpened ? (
+          <img
+            src="/accordianOpen.svg"
+            className="h-[20px] w-auto"
+            alt="logo"
+          />
+        ) : (
+          <img
+            src="/accordianClosed.svg"
+            className="h-[20px] w-auto"
+            alt="logo"
+          />
+        )}
+      </div>
+      <div
+        ref={contentElement}
+        style={{ height }}
+        className="overflow-hidden bg-gray-200 transition-all duration-200"
+      >
+        <p className="p-4">{content}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Service;
--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,8 +1,31 @@
 import React from "react";
-import Service from "./Service";
+import Service from "../Components/Service";
 
 const Services = () => {
-  return <div>Services</div>;
+  const data = [
+    {
+      title: "How much do i need to pay for this?",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nesciunt beatae debitis delectus pariatur nostrum maiores magni quibusdam officia tempore quis ea molestiae ducimus error nemo, tenetur possimus, earum illo.",
+    },
+    {
+      title: "Can i delete my account?",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nesciunt beatae debitis delectus pariatur nostrum maiores magni quibusdam officia tempore quis ea molestiae ducimus error nemo, tenetur possimus, earum illo. Impedit nesciunt beatae debitis delectus pariatur nostrum maiores magni quibusdam officia tempore quis ea molestiae ducimus error nemo, tenetur possimus, earum illo.",
+    },
+    {
+      title: "Is this accordion styled in Tailwind css?",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+  ];
+  return (
+    <div className="container mx-auto">
+      <h1 className="w-full text-center">Services</h1>
+      {data.map((item, key) => (
+        <Service title={item.title} content={item.content} key={item.title} />
+      ))}
+    </div>
+  );
 };
 
 export default Services;
