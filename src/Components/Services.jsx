896d926c3ecea7b1b46f8e73ebd396b778675f86
+ <andrewjohnjulian@gmail.com>
2023-05-26 11:43:21 -0600

    re-organized services page

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -30,12 +30,12 @@ const Services = () => {
     },
   ];
   return (
-    <div className="flex gap-5 bg-[#36b6ff] px-24 py-10">
-      <div className="grid h-[150px] w-1/2 px-6 pb-6 pt-2">
-        <h1 className="text-center text-4xl font-extrabold md:text-5xl xl:text-6xl">
+    <div className="my-10 grid w-full">
+      <div className=" mx-auto mb-5  w-3/4">
+        <h1 className="mb-2 text-center text-4xl font-extrabold md:text-5xl xl:text-6xl">
           Our Services
         </h1>
-        <p className="mt-5 h-[200px] text-xl md:top-0 md:text-center">
+        <p className=" h-auto text-xl md:top-0 md:text-center">
           We understand that each position is unique, and we will tailor our
           approach to each individual search. With a strong ever growing network
           of candidates, we work closely with our clients to understand their
@@ -45,7 +45,7 @@ const Services = () => {
         </p>
       </div>
 
-      <div className="w-1/2">
+      <div className="mx-auto w-[60%]">
         <div className="container mx-auto">
           {data.map((item, key) => (
             <Service
