7810d7863602255edbc7bd85ff919fdcc6e17223
+ <andrewjohnjulian@gmail.com>
2023-06-01 21:16:29 -0600

    trait array and map setup

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,6 +1,34 @@
 import React from "react";
+import Trait from "../Components/Trait";
+
+const traits = [
+  {
+    title: "Extensive Network",
+    description:
+      "We have cultivated strong relationships with top clients in the defense and space sector, giving us exclusive access to a wide range of job openings. Our vast network allows us to match talented professionals like you with the right opportunities that align with your skills and aspirations.",
+  },
+  {
+    title: "Personalized Approach",
+    description:
+      "At DB Recruitment Group, we believe in taking a personalized approach to understand your career goals, strengths, and preferences. We'll work closely with you to find the perfect fit, ensuring a rewarding and fulfilling career journey.",
+  },
+  {
+    title: "Industry Expertise",
+    description:
+      "Our team of experienced recruiters has in-depth knowledge of the defense and space industry. We stay up-to-date with the latest trends and requirements, enabling us to provide valuable insights and guidance throughout the hiring process.",
+  },
+  {
+    title: "Confidentiality",
+    description:
+      "We understand the importance of discretion and confidentiality when it comes to security clearances. Rest assured that your information will be handled with the utmost care and professionalism.",
+  },
+];
 
 const AboutUsPage = () => {
+  const displayTraits = traits.map((trait) => (
+    <Trait trait={trait} key={trait.title} />
+  ));
+
   return (
     <div
       id="about"
@@ -30,6 +58,9 @@ const AboutUsPage = () => {
             alt="DB Recruiting Logo"
           />
         </div>
+      </div>
+      <div className="mt-6 grid max-w-screen-xl px-10 py-1 md:flex md:px-4">
+        {displayTraits}
       </div>
     </div>
   );
