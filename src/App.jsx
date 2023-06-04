539dc2738119ce703a87228d5233eb1e9ea7cf3a
+ <andrewjohnjulian@gmail.com>
2023-06-03 22:26:29 -0600

    tried sample animation, needs revision

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "./App.css";
 import { Link } from "react-router-dom";
-
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import About from "./Components/About";
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "../App.css";
 
 const Contact = () => {
   return (
-    <div id="contact" className="my-[500px]">
+    <div id="contact" className="my-[200px]">
       Contact
     </div>
   );
