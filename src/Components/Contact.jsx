fdf5c074f07d429c2b54dc7cfea5ab252f248984
+ <andrewjohnjulian@gmail.com>
2023-06-04 22:00:07 -0600

    added shape, but needs styling

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -5,10 +5,7 @@ const Contact = () => {
   return (
     <div id="contact" className="relative">
       <div className="absolute inset-0 z-0 h-96 w-96 -skew-x-12 -skew-y-6 transform rounded-full bg-indigo-500"></div>
-      <div className="relative z-10">
-        <h1 className="text-4xl font-bold">Welcome to My React App!</h1>
-        <p className="text-lg">Lorem ipsum dolor sit amet...</p>
-      </div>
+      <div className="relative z-10">{/* Other content */}</div>
     </div>
   );
 };
--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -31,16 +31,19 @@ const Services = () => {
   ];
   return (
     <div id="services" className="grid w-full  py-10">
-      <div className="mx-auto w-[60%]">
-        <h1 className="mb-6 text-center text-4xl font-bold">OUR SERVICES</h1>
-        <div className="container mx-auto">
-          {data.map((item, key) => (
-            <Service
-              title={item.title}
-              content={item.content}
-              key={item.title}
-            />
-          ))}
+      <div className=" -skew-x-18 absolute left-[-200px] top-[1100px] z-0 h-[2000px] w-[2000px] -skew-y-12 transform rounded-full bg-[#36b6ff]"></div>
+      <div className="relative z-10">
+        <div className="mx-auto w-[60%]">
+          <h1 className="mb-6 text-center text-4xl font-bold">OUR SERVICES</h1>
+          <div className="container mx-auto">
+            {data.map((item, key) => (
+              <Service
+                title={item.title}
+                content={item.content}
+                key={item.title}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </div>
