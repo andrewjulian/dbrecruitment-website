20eba0597537b33686b5ee212014eff307a5a7dd
+ <andrewjohnjulian@gmail.com>
2023-06-19 22:36:39 -0600

    translated from html to react

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -44,21 +44,21 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <footer class="border-t-2 bg-gray-50 px-3 pt-4 lg:px-9">
-        <div class="row-gap-6 mb-8 grid gap-10 sm:grid-cols-2 lg:grid-cols-4">
-          <div class="sm:col-span-2">
-            <a href="#" class="inline-flex items-center">
+      <footer className="border-t-2 bg-gray-50 px-3 pt-4 lg:px-9">
+        <div className="row-gap-6 mb-8 grid gap-10 sm:grid-cols-2 lg:grid-cols-4">
+          <div className="sm:col-span-2">
+            <a href="#" className="inline-flex items-center">
               <img
                 src="https://mcqmate.com/public/images/logos/60x60.png"
                 alt="logo"
-                class="h-8 w-8"
+                className="h-8 w-8"
               />
-              <span class="ml-2 text-xl font-bold tracking-wide text-gray-800">
+              <span className="ml-2 text-xl font-bold tracking-wide text-gray-800">
                 Company Name
               </span>
             </a>
-            <div class="mt-6 lg:max-w-xl">
-              <p class="text-sm text-gray-800">
+            <div className="mt-6 lg:max-w-xl">
+              <p className="text-sm text-gray-800">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
                 felis mi, faucibus dignissim lorem id, imperdiet interdum
                 mauris. Vestibulum ultrices sed libero non porta. Vivamus
@@ -70,14 +70,14 @@ const Footer = () => {
             </div>
           </div>
 
-          <div class="flex flex-col gap-2 text-sm">
-            <p class="text-base font-bold tracking-wide text-gray-900">
+          <div className="flex flex-col gap-2 text-sm">
+            <p className="text-base font-bold tracking-wide text-gray-900">
               Popular Courses
             </p>
             <a href="#">UPSC - Union Public Service Commission</a>
             <a href="#">General Knowledge</a>
             <a href="#">MBA</a>
-            <p class="text-base font-bold tracking-wide text-gray-900">
+            <p className="text-base font-bold tracking-wide text-gray-900">
               Popular Topics
             </p>
             <a href="#">Human Resource Management</a>
@@ -86,33 +86,33 @@ const Footer = () => {
           </div>
 
           <div>
-            <p class="text-base font-bold tracking-wide text-gray-900">
+            <p className="text-base font-bold tracking-wide text-gray-900">
               COMPANY IS ALSO AVAILABLE ON
             </p>
-            <div class="flex items-center gap-1 px-2">
-              <a href="#" class="min-w-xl w-full">
+            <div className="flex items-center gap-1 px-2">
+              <a href="#" className="min-w-xl w-full">
                 <img
                   src="https://mcqmate.com/public/images/icons/playstore.svg"
                   alt="Playstore Button"
-                  class="h-10"
+                  className="h-10"
                 />
               </a>
               <a
-                class="min-w-xl w-full"
+                className="min-w-xl w-full"
                 href="https://www.youtube.com/channel/UCo8tEi6SrGFP8XG9O0ljFgA"
               >
                 <img
                   src="https://mcqmate.com/public/images/icons/youtube.svg"
                   alt="Youtube Button"
-                  class="h-28"
+                  className="h-28"
                 />
               </a>
             </div>
-            <p class="text-base font-bold tracking-wide text-gray-900">
+            <p className="text-base font-bold tracking-wide text-gray-900">
               Contacts
             </p>
-            <div class="flex">
-              <p class="mr-1 text-gray-800">Email:</p>
+            <div className="flex">
+              <p className="mr-1 text-gray-800">Email:</p>
               <a href="#" title="send email">
                 admin@company.com
               </a>
@@ -120,15 +120,15 @@ const Footer = () => {
           </div>
         </div>
 
-        <div class="flex flex-col-reverse justify-between border-t pb-10 pt-5 lg:flex-row">
-          <p class="text-sm text-gray-600">
+        <div className="flex flex-col-reverse justify-between border-t pb-10 pt-5 lg:flex-row">
+          <p className="text-sm text-gray-600">
             © Copyright 2023 Company. All rights reserved.
           </p>
-          <ul class="mb-3 flex flex-col space-y-2 sm:flex-row sm:space-x-5 sm:space-y-0 lg:mb-0">
+          <ul className="mb-3 flex flex-col space-y-2 sm:flex-row sm:space-x-5 sm:space-y-0 lg:mb-0">
             <li>
               <a
                 href="#"
-                class="hover:text-deep-purple-accent-400 text-sm text-gray-600 transition-colors duration-300"
+                className="hover:text-deep-purple-accent-400 text-sm text-gray-600 transition-colors duration-300"
               >
                 Privacy &amp; Cookies Policy
               </a>
@@ -136,7 +136,7 @@ const Footer = () => {
             <li>
               <a
                 href="#"
-                class="hover:text-deep-purple-accent-400 text-sm text-gray-600 transition-colors duration-300"
+                className="hover:text-deep-purple-accent-400 text-sm text-gray-600 transition-colors duration-300"
               >
                 Disclaimer
               </a>
