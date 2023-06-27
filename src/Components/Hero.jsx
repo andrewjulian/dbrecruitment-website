bb47302875e734a1b4ee543c854763ee710620aa
+ <andrewjohnjulian@gmail.com>
2023-06-27 16:49:43 -0600

    missing hero image attempt

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import stock from "../assets/stock.jpg";
 
 const Hero = () => {
   return (
-    <div id="hero" className="relative z-10 bg-white p-16 text-center">
-      <div className="absolute left-0 top-0 h-full w-full bg-stock bg-cover bg-fixed bg-no-repeat opacity-[.15] "></div>
+    <div
+      id="hero"
+      className="relative z-10 bg-white p-16 text-center"
+    >
+      <div className="absolute left-0 top-0 h-full w-full bg-[stock] bg-cover bg-fixed bg-no-repeat opacity-[.15] "></div>
       <div className="flex min-h-[90%] items-center justify-center">
         <div className="mx-auto mt-5 max-w-screen-xl px-4 py-1 text-center">
           <div className="mr-auto place-self-center lg:col-span-7">
